06ed5bce447faae7bcbbc24bda8254ad1b67b2c4
Elias Kristmansson <elias.kristmansson97@gmail.com>
2025-04-24 12:13:45 +0200

    fixed tabs

--- reactproject1/src/components/Topbar.jsx
+++ reactproject1/src/components/Topbar.jsx
@@ -2,10 +2,12 @@ import React from "react";
 
 export default function Topbar() {
   return (
-    <div className="topbar">
-      <div className="tab">Projekt 1</div>
-      <div className="tab">Projekt 2</div>
-      <div className="tab">Projekt 3</div>
+      <div className="topbar">
+          <div className="topbar-content">
+              <div className="tab">Projekt 1</div>
+              <div className="tab">Projekt 2</div>
+              <div className="tab">Projekt 3</div>
+          </div>
     </div>
   );
 }
